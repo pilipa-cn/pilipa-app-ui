4605f4ac35ec5f6eeed1a4a7c91a3f399dc87a8f
BeanSoft <beansoft@126.com>
2017-09-04 18:56:24 +0800

    add global styles

---
+++ Example/lib/style/Size.js
@@ -0,0 +1,6 @@
+export const SIZE_TIP = 36/2;//  个别使用，用于提示行文字
+export const SIZE_POPUP = 34/2;//  个别使用，用于弹窗文字
+export const SIZE_BUTTON = 32/2;//  大面积使用，用于未点击状态的按钮
+export const SIZE_PRIMARY_TEXT = 30/2;//  大面积使用，用于正文重要性文字
+export const SIZE_SECONDARY_TEXT = 28/2;//  大面积使用，用于正文次要文字
+export const SIZE_INPUT_TEXT = 24/2;//  小面积使用，用于输入框内文字及弱提示性文字
